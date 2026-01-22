67ad72efa826358b24cca27263ac6ad546ec098f
kirtan-zt <kirtand@zignuts.com>
2026-01-22 13:46:43 +0530

    Decrease the score threshold for better results

--- AI_agents/Assignment_2/Major Project Job board API/src/crud/ai_search.py
+++ AI_agents/Assignment_2/Major Project Job board API/src/crud/ai_search.py
@@ -50,7 +50,7 @@ retriever = vector_store.as_retriever(
     search_type="similarity_score_threshold",
     search_kwargs={
         "k": 5, 
-        "score_threshold": 0.45
+        "score_threshold": 0.25
     }
 )
 
